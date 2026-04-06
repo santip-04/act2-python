256cadc05fa2894e3b73d9d39ae285bbb6f2f093
santip-04 <pastorsantiago99@gmail.com>
2026-04-06 11:01:21 -0300

    Finalizado ejercicio 10, debo corregir un problema de la función: imprimir_tabla_final

--- src/funciones.py
+++ src/funciones.py
@@ -17,3 +17,53 @@ def inicializar_participantes (nombres):
             "rondas": []
         }
     return data
+
+def procesar_competencia(rounds):
+    nombres = list(rounds[0]['scores'].keys())
+    data = inicializar_participantes(nombres)
+
+    for i, ronda in enumerate(rounds, 1):
+        print(f"\nRonda {i} - {ronda['theme']}:")
+
+        resultados = calcular_puntaje(ronda['scores'])
+
+        ganador = max(resultados, key=resultados.get)
+        print(f"Ganador: {ganador} ({resultados[ganador]} pts)")
+
+        data[ganador]["rondas_ganadas"] += 1
+
+        for nombre, puntaje in resultados.items():
+            data[nombre]["total"] += puntaje
+            data[nombre]["rondas"].append(puntaje)
+
+            if puntaje > data[nombre]["mejor_ronda"]:
+                data[nombre]["mejor_ronda"] = puntaje
+
+        # Imprime tabla parcial
+        imprimir_tabla(data)
+
+    return data
+
+def imprimir_tabla(data):
+    print("\nTabla de posiciones:")
+
+    ordenados = sorted(data.items(), key=lambda x: x[1]["total"], reverse=True)
+
+    for nombre, stats in ordenados:
+        print(f"{nombre}: {stats['total']} pts")
+
+def imprimir_tabla_final(data):
+    
+    print("\nTabla final:\n")
+
+    ordenados = sorted(data.items(), key=lambda x: x[1]["total"], reverse=True)
+
+    print(f"{'Participante':<12} {'Puntaje':<10} {'Ganadas':<10} {'Mejor':<10} {'Promedio'}")
+    print("-" * 55)
+
+    for nombre, stats in ordenados:
+        promedio = sum(stats["rondas"]) / len(stats["rondas"])
+
+        print(f"{nombre:<12} {stats['total']:<10} {stats['rondas_ganadas']:<10} {stats['mejor_ronda']:<10} {round(promedio,1)}")
+
+    
